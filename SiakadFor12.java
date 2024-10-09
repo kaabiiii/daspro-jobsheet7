2ad6a503665cf055e12b245f6ecb4c01db997826
kaabiiii <kaabimrfuad1@gmail.com>
2024-10-09 18:09:42 +0700

    Add files via upload

--- SiakadFor12.java
+++ SiakadFor12.java
@@ -6,6 +6,7 @@ public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
 
 double nilai, tertinggi = 0, terendah = 100;
+int lulus = 0, tdklulus = 0;
 
 for (int i = 1 ; i <= 10; i++){
     System.out.println("Masukkan nilai mahasiswa ke " + i + ": ");
@@ -13,13 +14,21 @@ for (int i = 1 ; i <= 10; i++){
     if (nilai > tertinggi) {
         tertinggi= nilai;
         
-    }if (nilai<terendah) {
-        terendah = nilai;        
-    }
+    }if (nilai < terendah) {
+        terendah = nilai;    
+
+    }if (nilai >= 60) {
+            lulus++;
+        }else {
+            tdklulus++;
+            
+        }
  
 }
 System.out.println("Nilai tertinggi: " + tertinggi);
 System.out.println("Nilai terendah: " + terendah);
+System.out.println("Jumlah siswa yang lulus: "+ lulus);
+System.out.println("Jumlah siswa yang tidak lulus: " + tdklulus);
 }
 
 
